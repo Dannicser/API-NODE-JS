bff29b53cae167d1b29f90762ccc404b584f4d04
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-04-10 17:21:12 +0300

    feat: add ExeptionFilters

--- src/app.ts
+++ src/app.ts
@@ -3,28 +3,36 @@ import express, { Express } from "express";
 
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from "./user/user.controller";
+import { ExeptionFilters } from "./errors/exeption.filter";
 
 export class App {
   app: Express;
   server: Server;
   port: number;
+  exeptionFilter: ExeptionFilters;
   loggerService: LoggerService;
   userController: UserController;
 
-  constructor(logger: LoggerService, userController: UserController) {
+  constructor(logger: LoggerService, userController: UserController, exeptionFilter: ExeptionFilters) {
     this.app = express();
     this.port = 5000;
     this.loggerService = logger;
     this.userController = userController;
+    this.exeptionFilter = exeptionFilter;
   }
 
   useRoutes() {
     this.app.use("/users", this.userController.router);
   }
 
+  useExceptionFilters() {
+    this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+  }
+
   public async init() {
     this.server = this.app.listen(this.port);
     this.useRoutes();
+    this.useExceptionFilters();
     this.loggerService.log(`server has been started on ${this.port} port`);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { App } from "./app";
+import { ExeptionFilters } from "./errors/exeption.filter";
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from "./user/user.controller";
 
 async function bootstrap() {
   const logger = new LoggerService();
-  const app = new App(logger, new UserController(logger)); // яркий пример dependency injection, строю дерево зависимостей
+  const app = new App(logger, new UserController(logger), new ExeptionFilters(logger)); // яркий пример dependency injection, строю дерево зависимостей
 
   await app.init();
 }
